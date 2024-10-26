306f75bd35c372eb61959fa22b15a3dc405d7bbc
Arish <thermic.arish@gmail.com>
2024-10-26 21:09:09 +0530

    Add Rails Admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin/arish-me', as: 'rails_admin'
+  devise_for :users, skip: %i[registrations passwords confirmations unlocks]
   get 'up' => 'rails/health#show', as: :rails_health_check
   # get '/rails/active_storage/blobs/:signed_id/*filename' => 'active_storage/blobs#show', as: 'rails_blob'
   namespace :api, defaults: { format: :json } do
