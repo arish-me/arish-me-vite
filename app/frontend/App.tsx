0817c7ba1579d2586cf3d00ea5b5e98e976b06cf
Arish <thermic.arish@gmail.com>
2024-11-08 11:37:37 +0530

    Google Analytics

--- app/frontend/App.tsx
+++ app/frontend/App.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from "@/context/ThemeContext";
 import AppRouter from '@/AppRouter';
 
+
 const App = () => {
+useEffect(() => {
+    const script = document.createElement("script");
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`;
+    script.async = true;
+    document.head.appendChild(script);
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+        window.dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+    gtag("config", import.meta.env.VITE_GOOGLE_ANALYTICS_ID);
+}, []);
   return (
     <React.StrictMode>
     <ThemeProvider>
